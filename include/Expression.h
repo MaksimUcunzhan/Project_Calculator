c3ced7cd1c1fa93330eecbece182ed4de5a914fc
Maksim Uchunzhyan <maksimucunzan@gmail.com>
2026-03-10 20:44:47 +0300

    implement the base class Expression, virtual destructor

--- include/Expression.h
+++ include/Expression.h
@@ -6,3 +6,24 @@
 #define PROJECT_CALCULATOR_EXPRESSION_H
 
 #endif //PROJECT_CALCULATOR_EXPRESSION_H
+
+#pragma once
+#include <memory>
+#include <string>
+#include "Complex.h"
+
+// abstract base class
+class Expression {
+public:
+    // virtual destructor
+    virtual ~Expression() = default;
+
+    // calculating value
+    virtual Complex evaluate() const = 0;
+
+    // string
+    virtual std::string toString() const = 0;
+
+    // Clonability (for copy tree)
+    virtual std::unique_ptr<Expression> clone() const = 0;
+};
