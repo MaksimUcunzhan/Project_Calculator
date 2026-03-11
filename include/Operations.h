da4211940b84a4c67e4ea27012f176a4960c9124
mambur <glebglebka.07@gmail.com>
2026-03-11 14:12:42 +0300

    created a base class for binary operations

--- include/Operations.h
+++ include/Operations.h
@@ -1,8 +1,28 @@
-//
-// Created by Максим Учунжян on 09.03.2026.
-//
+#pragma once
+#include <memory>
+#include <string>
+#include "Expression.h"
 
-#ifndef PROJECT_CALCULATOR_OPERATIONS_H
-#define PROJECT_CALCULATOR_OPERATIONS_H
+// created a base class for binary operations
+class BinaryOperation : public Expression {
+protected:
+    std::unique_ptr<Expression> left;
+    std::unique_ptr<Expression> right;
 
-#endif //PROJECT_CALCULATOR_OPERATIONS_H
+public:
+    // created a constructor
+    BinaryOperation(std::unique_ptr<Expression> l, std::unique_ptr<Expression> r) : left(std::move(l)), right(std::move(r)) {
+    }
+    virtual Complex applyOperations(const Complex& l,const Complex& r) const = 0;
+    virtual std::string getOperatorSymbol() const = 0;
+
+    Complex evaluate() const override {
+        return applyOperations(left->evaluate(),right->evaluate());
+    }
+    std::string toString() const override {
+        return "(" + left->toString() + " " + getOperatorSymbol() +
+               " " + right->toString() + ")";
+    }
+
+
+};
