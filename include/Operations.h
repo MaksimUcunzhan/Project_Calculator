d886f075ffd026ee498f852ac5809c689a5977c1
mambur <glebglebka.07@gmail.com>
2026-03-12 13:35:42 +0300

    corrected class Binary Operations for header file

--- include/Operations.h
+++ include/Operations.h
@@ -11,18 +11,16 @@ protected:
 
 public:
     // created a constructor
-    BinaryOperation(std::unique_ptr<Expression> l, std::unique_ptr<Expression> r) : left(std::move(l)), right(std::move(r)) {
-    }
-    virtual Complex applyOperations(const Complex& l,const Complex& r) const = 0;
+    BinaryOperation(std::unique_ptr<Expression> l, std::unique_ptr<Expression> r);
+
+    virtual ~BinaryOperation() = default;
+
+
+    virtual Complex applyOperations(const Complex &l, const Complex &r) const = 0;
+
     virtual std::string getOperatorSymbol() const = 0;
 
-    Complex evaluate() const override {
-        return applyOperations(left->evaluate(),right->evaluate());
-    }
-    std::string toString() const override {
-        return "(" + left->toString() + " " + getOperatorSymbol() +
-               " " + right->toString() + ")";
-    }
+    Complex evaluate() const override;
 
-
+    std::string toString() const override;
 };
