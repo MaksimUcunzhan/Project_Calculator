4dfe8ff657c5d96227971a69b6d1c20e963c3da6
mambur <glebglebka.07@gmail.com>
2026-03-12 18:34:40 +0300

    Implementation fo Variable

--- src/Variable.cpp
+++ src/Variable.cpp
@@ -1,1 +1,29 @@
 #include "Variable.h"
+#include <stdexcept>
+
+class UndefinedVariableException : public std::runtime_error {
+public:
+    explicit UndefinedVariableException(const std::string &msg) : std::runtime_error(msg) {
+    }
+};
+
+Variable::Variable(const std::string &n) : name(n) {
+}
+
+Complex Variable::evaluate() const {
+    auto value = globalContext.getVariable(name);
+    if (!value.has_value()) {
+        throw UndefinedVariableException("Variable '" + name + "' is not defined");
+    }
+    return value.value();
+}
+
+std::string Variable::toString() const {
+    return name;
+}
+
+std::unique_ptr<Expression> Variable::clone() const {
+    return std::make_unique<Variable>(name);
+}
+
+
