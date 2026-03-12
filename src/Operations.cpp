ed3f13c8e959d8027f4cd561f0136db5085ee0ef
mambur <glebglebka.07@gmail.com>
2026-03-12 17:23:25 +0300

    Implementation of MulOperation

--- src/Operations.cpp
+++ src/Operations.cpp
@@ -3,9 +3,11 @@
 BinaryOperation::BinaryOperation(std::unique_ptr<Expression> l, std::unique_ptr<Expression> r) : left(std::move(l)),
     right(std::move(r)) {
 }
+
 Complex BinaryOperation::evaluate() const {
-    return applyOperations(left->evaluate(),right->evaluate());
+    return applyOperations(left->evaluate(), right->evaluate());
 }
+
 std::string BinaryOperation::toString() const {
     return "(" + left->toString() + " " + getOperatorSymbol() +
            " " + right->toString() + ")";
@@ -13,16 +15,25 @@ std::string BinaryOperation::toString() const {
 
 
 Complex AddOperation::applyOperations(const Complex &l, const Complex &r) const {
-    return l+r;
+    return l + r;
 }
+
 std::string AddOperation::getOperatorSymbol() const {
     return "+";
 }
+
 std::unique_ptr<Expression> AddOperation::clone() const {
-    return std::make_unique<AddOperation>(left->clone(),right->clone());
+    return std::make_unique<AddOperation>(left->clone(), right->clone());
 }
 
+Complex MulOperations::applyOperations(const Complex &l, const Complex &r) const {
+    return l * r;
+}
 
+std::string MulOperations::getOperatorSymbol() const {
+    return "*";
+}
 
-
-
+std::unique_ptr<Expression> MulOperations::clone() const {
+    return std::make_unique<MulOperations>(left->clone(), right->clone());
+}
