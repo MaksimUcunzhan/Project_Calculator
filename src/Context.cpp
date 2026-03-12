73845ba5736f4a2755495740f18bacea6070e270
mambur <glebglebka.07@gmail.com>
2026-03-12 18:14:03 +0300

    Implementation of Context.cpp

--- src/Context.cpp
+++ src/Context.cpp
@@ -1,3 +1,19 @@
-//
-// Created by Глеб Карицкий on 12.03.2026.
-//
+#include "Context.h"
+
+Context globalContext;
+
+void Context::setVariable(const std::string &name, const Complex &value) {
+    variables[name] = value;
+}
+
+std::optional<Complex> Context::getVariable(const std::string &name) const {
+    auto it = variables.find(name);
+    if (it != variables.end()) {
+        return it->second;
+    }
+    return std::nullopt;
+}
+bool Context::hasVariable(const std::string &name) const {
+    return variables.find(name) != variables.end();
+}
+
