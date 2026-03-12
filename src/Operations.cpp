4bbe3ad40a661edcbc83a1bfb37d8a08363b7e09
mambur <glebglebka.07@gmail.com>
2026-03-12 13:51:26 +0300

    Implementation of BinaryOperation

--- src/Operations.cpp
+++ src/Operations.cpp
@@ -1,3 +1,14 @@
-//
-// Created by Максим Учунжян on 09.03.2026.
-//
+#include "Operations.h"
+
+BinaryOperation::BinaryOperation(std::unique_ptr<Expression> l, std::unique_ptr<Expression> r) : left(std::move(l)),
+    right(std::move(r)) {
+}
+Complex BinaryOperation::evaluate() const {
+    return applyOperations(left->evaluate(),right->evaluate());
+}
+std::string BinaryOperation::toString() const {
+    return "(" + left->toString() + " " + getOperatorSymbol() +
+           " " + right->toString() + ")";
+}
+
+
