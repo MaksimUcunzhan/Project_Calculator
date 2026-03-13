a8059470fc1282e117055812381fd7070a005b67
Kayo-00 <tigrenochekson2007@gmail.com>
2026-03-13 03:52:36 -0700

    start making logger

--- include/Logger.h
+++ include/Logger.h
@@ -1,8 +1,30 @@
-//
-// Created by Максим Учунжян on 09.03.2026.
-//
+#pragma once
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <ctime>
+#include "Exceptions.h"
 
-#ifndef PROJECT_CALCULATOR_LOGGER_H
-#define PROJECT_CALCULATOR_LOGGER_H
 
-#endif //PROJECT_CALCULATOR_LOGGER_H
+class Logger {
+public:
+    explicit Logger(const std::string& filename)
+        : filename_(filename), active_(false)
+    {
+        file_.open(filename_, std::ios::out | std::ios::app);
+        if (!file_.is_open()) {
+            throw FileIOException(filename_);
+        }
+        active_ = true;
+        write("[SESSION START]");
+    }
+
+    ~Logger() {
+        if (active_) {
+            try {
+                write("[SESSION END]");
+                file_.close();
+            } catch (...) {}
+        }
+    }
+};
